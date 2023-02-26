52c630618016aac69f58c6315ac3cf9599449ac7
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-02-26 18:13:43 +0200

    Done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitCreatePromises);
+
+function submitCreatePromises(event) {
+  event.preventDefault();
+  let delay = Number(form.delay.value);
+  let step = Number(form.step.value);
+  let amount = Number(form.amount.value);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
